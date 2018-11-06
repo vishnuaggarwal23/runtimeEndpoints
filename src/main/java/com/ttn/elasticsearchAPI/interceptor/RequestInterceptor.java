7317aaadc99dec6ba4b6ab96dbc8222149bc2413
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-11-06 13:40:42 +0530

    corrected pre-handler of interceptor

--- src/main/java/com/ttn/elasticsearchAPI/interceptor/RequestInterceptor.java
+++ src/main/java/com/ttn/elasticsearchAPI/interceptor/RequestInterceptor.java
@@ -2,6 +2,7 @@ package com.ttn.elasticsearchAPI.interceptor;
 
 
 import com.ttn.elasticsearchAPI.util.ConfigHelper;
+import groovy.lang.Closure;
 import groovy.util.ConfigObject;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,9 @@ public class RequestInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         boolean validPreHandling = false;
         ConfigObject processorConfig = configHelper.getProcessorMap();
-        if (!(processorConfig.isEmpty() || ((ConfigObject) processorConfig.get("post")).isEmpty())) {
-            Object closure = ((ConfigObject) processorConfig.get("post")).get("json");
+
+        if (!processorConfig.isEmpty() && !((ConfigObject) processorConfig.get("pre")).isEmpty()) {
+            Object closure = ((Closure) ((ConfigObject) processorConfig.get("pre")).get("json")).call(request, response, handler);
             if (closure instanceof Boolean) {
                 validPreHandling = (boolean) closure;
             }
